712630fa5e53c4b89ba0f962932750c2e49147ae
Charlie Zhao <charles.zhao5461@gmail.com>
2024-10-07 17:05:41 +1100

    Use vstack to simplify logic for alert list item positioning

--- YaoguaiV3/Views/Alert/AlertList.swift
+++ YaoguaiV3/Views/Alert/AlertList.swift
@@ -11,29 +11,31 @@ struct AlertList: View {
 	var alertManager = AlertManager.shared
 	
 	var body: some View {
-		ForEach(alertManager.alerts) { alert in
-			let index = alertManager.alerts.firstIndex(where: { $0.id == alert.id }) ?? 0
-			HStack {
-				Image(systemName: alert.type.icon) // Use icon from ToastType
-					.foregroundColor(.white)
+		VStack {
+			ForEach(alertManager.alerts) { alert in
+				let index = alertManager.alerts.firstIndex(where: { $0.id == alert.id }) ?? 0
 				
-				Text(alert.message)
-					.bold()
-					.foregroundColor(.white)
-			}
-			.padding()
-			.background(alert.type.backgroundColor) // Use background color from ToastType
-			.clipShape(RoundedRectangle(cornerRadius: 8.0))
-			.onTapGesture {
-				withAnimation {
-					alertManager.removeAlert(alert)
+				HStack {
+					Image(systemName: alert.type.icon) // Use icon from ToastType
+						.foregroundColor(.white)
+					
+					Text(alert.message)
+						.bold()
+						.foregroundColor(.white)
 				}
+				.padding()
+				.background(alert.type.backgroundColor) // Use background color from ToastType
+				.clipShape(RoundedRectangle(cornerRadius: 8.0))
+				.onTapGesture {
+					withAnimation {
+						alertManager.removeAlert(alert)
+					}
+				}
+				.transition(.asymmetric(insertion: .move(edge: .bottom), removal: .move(edge: .bottom)).combined(with: .opacity))
+				.animation(.easeInOut, value: alertManager.alerts.count)
 			}
-			.frame(height: 70)
-			.offset(y: -CGFloat(index * 70))
-			.transition(.asymmetric(insertion: .move(edge: .top), removal: .move(edge: .bottom)).combined(with: .opacity))
-			.animation(.easeInOut, value: alertManager.alerts.count)
 		}
+		.padding()
 	}
 }
 
